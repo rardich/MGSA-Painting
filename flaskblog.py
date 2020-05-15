12ac7b9827fb2ff0bf650d7f736874faadf97479
Richard <richard.jin.zhang@gmail.com>
2020-05-14 18:23:02 -0700

    Created gallery navigation and pagination

---
+++ flaskblog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for, flash, redirect
+
+app = Flask(__name__)
+
+# Positions, sizes, and links for art in galleries
+artwork = {
+    "room1": [],
+    "room2": [],
+    "room3": [],
+    "room4": [],
+    "room5": [],
+    "room6": [],
+    "room7": [],
+    "room8": []
+}
+
+@app.route('/<id>')
+def gallery(id):
+    return render_template('gallery.html', title=f"Room {id}", page = int(id), totalpages = 8, background=f"../static/galleries/room {id}.jpg", artwork=artwork[f"room{id}"])
+
+@app.route('/artists')
+def artists():
+    return render_template('about.html', title="Artists")
+
+@app.route('/<art>')
+def art():
+    return
+
+if __name__ == '__main__':
+    app.run(debug=True)
