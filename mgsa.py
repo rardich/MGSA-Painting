cdb83e733991b40a95c6feb69fc13c14f2b3f4e5
Richard <richard.jin.zhang@gmail.com>
2020-05-21 18:04:47 -0700

    Deployment port

--- mgsa.py
+++ mgsa.py
@@ -202,5 +202,5 @@ def art(art):
     return render_template('art.html', art=art, profiles=profiles, artwork=artwork)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
